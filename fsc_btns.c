108e00dda3ce49c889451f1f61939f65ae4caddf
khnz <khnz@4a459e26-1d7e-42ee-bc08-29a454924080>
2007-06-09 03:57:43 +0000

    allow userspace to reprogram the keycodes   merged from branch/fsc_btns@62   add MSC_RAW and MSC_SCAN start collection of reset sequences   wrong settings can broke the keyboard autodetect keymap (with acpi device id) some cleanups (but to much left ;)
    git-svn-id: https://fjbtndrv.svn.sourceforge.net/svnroot/fjbtndrv/trunk@64 4a459e26-1d7e-42ee-bc08-29a454924080

--- fsc_btns.c
+++ fsc_btns.c
@@ -53,44 +53,56 @@
 #define FJBTNS_DOCK_READ	0xfd65
 #define FJBTNS_DOCK_READ_STATE	0x52
 
+#define FJBTNS_BASE		fscbtns.address
+#define FJBTNS_ADDRESS_PORT	FJBTNS_BASE
+#define FJBTNS_RESET_PORT	FJBTNS_BASE+2
+#define FJBTNS_DATA_PORT	FJBTNS_BASE+4
+#define FJBTNS_STATUS_PORT	FJBTNS_BASE+6
 
-struct keymap_entry {				/* keymap_entry */
-	unsigned int mask;
-	unsigned int code;
+#define default_keymap keymap_Tseries
+static unsigned int keymap_Tseries[16] = {
+  /* 0x0001 */	KEY_UNKNOWN,
+  /* 0x0002 */	KEY_UNKNOWN,
+  /* 0x0004 */	KEY_UNKNOWN,
+  /* 0x0008 */	KEY_UNKNOWN,
+  /* 0x0010 */	KEY_SCROLLDOWN,
+  /* 0x0020 */	KEY_SCROLLUP,
+  /* 0x0040 */	KEY_DIRECTION,
+  /* 0x0080 */	KEY_FN,
+  /* 0x0100 */	KEY_BRIGHTNESSUP,
+  /* 0x0200 */	KEY_BRIGHTNESSDOWN,
+  /* 0x0400 */	KEY_DISPLAYTOGGLE,
+  /* 0x0800 */	KEY_UNKNOWN,
+  /* 0x1000 */	KEY_UNKNOWN,
+  /* 0x2000 */	KEY_UNKNOWN,
+  /* 0x4000 */	KEY_UNKNOWN,
+  /* 0x8000 */	KEY_MENU
 };
 
-static struct keymap_entry keymap_t4010[] = {
-	{ 0x0010, KEY_SCROLLDOWN },
-	{ 0x0020, KEY_SCROLLUP },
-	{ 0x0040, KEY_DIRECTION },
-	{ 0x0080, KEY_FN },
-	{ 0x0100, KEY_BRIGHTNESSUP },
-	{ 0x0200, KEY_BRIGHTNESSDOWN },
-	{ 0x8000, KEY_MENU },
-	{ 0x0000, 0},
-};
-
-static struct keymap_entry keymap_stylistic[] = {
-	{ 0x0010, KEY_PRINT },
-	{ 0x0020, KEY_BACKSPACE },
-	{ 0x0040, KEY_SPACE },
-	{ 0x0080, KEY_ENTER },
-	{ 0x0100, KEY_BRIGHTNESSUP },
-	{ 0x0200, KEY_BRIGHTNESSDOWN },
-	{ 0x0400, KEY_UP },
-	{ 0x0800, KEY_DOWN },
-	{ 0x1000, KEY_PAGEUP },
-	{ 0x2000, KEY_PAGEDOWN },
-	{ 0x4000, KEY_FN },
-	{ 0x8000, KEY_MENU },
-	{ 0x0000, 0},
+static unsigned int keymap_Stylistic[16] = {
+  /* 0x0001 */	KEY_UNKNOWN,
+  /* 0x0002 */	KEY_UNKNOWN,
+  /* 0x0004 */	KEY_UNKNOWN,
+  /* 0x0008 */	KEY_UNKNOWN,
+  /* 0x0010 */	KEY_PRINT,
+  /* 0x0020 */	KEY_BACKSPACE,
+  /* 0x0040 */	KEY_SPACE,
+  /* 0x0080 */	KEY_ENTER,
+  /* 0x0100 */	KEY_BRIGHTNESSUP,
+  /* 0x0200 */	KEY_BRIGHTNESSDOWN,
+  /* 0x0400 */	KEY_UP,
+  /* 0x0800 */	KEY_DOWN,
+  /* 0x1000 */	KEY_PAGEUP,
+  /* 0x2000 */	KEY_PAGEDOWN,
+  /* 0x4000 */	KEY_FN,
+  /* 0x8000 */	KEY_MENU
 };
 
 static struct fscbtns_t {				/* fscbtns_t */
 	unsigned int interrupt;
 	unsigned int address;
 
-	struct keymap_entry *keymap;
+	unsigned int *keymap;
 	int orientation;
 	int docked;
 
@@ -100,25 +112,30 @@ static struct fscbtns_t {				/* fscbtns_t */
 	struct acpi_device *adev;
 #endif
 
+	u8 *reset_sequence;
+
 	struct input_dev *idev;
 	char idev_phys[16];
-} fscbtns = {
-
+} fscbtns
 #ifndef CONFIG_ACPI
 	/* XXX: is this always true ??? */
-	.interrupt = 5,
-	.address = 0xfd70,
+	= { .interrupt = 5, .address = 0xfd70 }
 #endif
-       	.keymap = keymap_t4010,
-};
+;
 
 static unsigned int repeat_rate = 16;
 static unsigned int repeat_delay = 500;
 static unsigned int user_keymap = 0;		// TODO: autodetect (acpi/dmi?)
 
+#ifdef __ONLY_A_COLLECTION_OF_CODES__
+typedef u8 reset_sequence[3];
+reset_sequence reset_t4010 = { 0x02, 0x04, 0x05 };
+reset_sequence reset_p1610 = { 0x02, 0x00, 0x05 };
+reset_sequence reset_jan   = { 0x02, 0x0c, 0x05 };
+#endif
 
-#define IOREADB(offset)		inb(fscbtns.address+(offset));
-#define IOWRITEB(data, offset)	outb((data), fscbtns.address+(offset));
+#define IOREADB(offset)		inb(offset)
+#define IOWRITEB(data, offset)	outb(data, offset)
 
 #define pr_err(fmt, arg...) \
 	printk(KERN_ERR fmt ,##arg)
@@ -129,9 +146,9 @@ static unsigned int user_keymap = 0;		// TODO: autodetect (acpi/dmi?)
 #ifdef DEBUG_IO
 
 u8 ioreadb(int offset) {
-	u8 data = inb(fscbtns.address + offset);
-	pr_debug("IOREADB(%d): 0x%02x\n", offset, data);
-	pr_debug("IOREADB(6): 0x%02x\n", inb(fscbtns.address + 6));
+	u8 data = inb(offset);
+	pr_debug("IOREADB(%d): 0x%02x, Status=0x%02x\n",
+			offset, data, inb(FJBTNS_STATUS_PORT));
 	return data;
 }
 #undef  IOREADB
@@ -140,9 +157,9 @@ u8 ioreadb(int offset) {
 
 void iowriteb(u8 data, int offset)
 {
-	outb(data, fscbtns.address + offset);
-	pr_debug("IOWRITEB(%d): 0x%02x\n", offset, data);
-	pr_debug("IOREADB(6): 0x%02x\n", inb(fscbtns.address + 6));
+	outb(data, offset);
+	pr_debug("IOWRITEB(%d): 0x%02x, Status=0x%02x\n",
+			offset, data, inb(FJBTNS_STATUS_PORT));
 }
 #undef  IOWRITEB
 #define IOWRITEB(data, offset) iowriteb(data, offset)
@@ -165,14 +182,27 @@ static void dump_regs(void)
 #endif /* DEBUG_IO */
 #endif /* DEBUG */
 
+inline u8 fscbtns_read_register(const u8 addr)
+{
+	IOWRITEB(addr, FJBTNS_ADDRESS_PORT);
+	return IOREADB(FJBTNS_DATA_PORT);
+}
+
+#if 0 /* avoid warning */
+inline void fscbtns_write_register(const u8 addr, const u8 data)
+{
+	IOWRITEB(addr, FJBTNS_ADDRESS_PORT);
+	IOWRITEB(data, FJBTNS_DATA_PORT);
+}
+#endif
 
 /*** INPUT ********************************************************************/
 
 static int input_fscbtns_setup(void)
 {
-	struct keymap_entry *key;
 	struct input_dev *idev;
 	int error;
+	int x;
 
 	snprintf(fscbtns.idev_phys, sizeof(fscbtns.idev_phys),
 			"fsc/input0");
@@ -189,10 +219,22 @@ static int input_fscbtns_setup(void)
 	idev->id.version = 0x0101;
 	idev->cdev.dev = &(fscbtns.pdev->dev);
 
+	if(!fscbtns.keymap) {
+		pr_info("A: NO KEYMAP!");
+		fscbtns.keymap = default_keymap;
+	}
+
+	idev->keycode = fscbtns.keymap;
+	idev->keycodesize = sizeof(unsigned int);
+	idev->keycodemax  = ARRAY_SIZE(default_keymap);
+
 	set_bit(EV_REP, idev->evbit);
+	set_bit(EV_MSC, idev->evbit);
+	set_bit(MSC_RAW, idev->mscbit);
+	set_bit(MSC_SCAN, idev->mscbit);
 	set_bit(EV_KEY, idev->evbit);
-	for(key = fscbtns.keymap; key->mask; key++)
-		set_bit(key->code, idev->keybit);
+	for(x = 0; x < ARRAY_SIZE(default_keymap); x++)
+		set_bit(fscbtns.keymap[x], idev->keybit);
 
 	set_bit(EV_SW, idev->evbit);
 	set_bit(SW_TABLET_MODE, idev->swbit);
@@ -217,68 +259,78 @@ static void fscbtns_set_repeat_rate(int delay, int period)
 	fscbtns.idev->rep[REP_PERIOD] = period;
 }
 
-static void fscbtns_event(void)
+inline void fscbtns_handle_rotating(void)
 {
-	u8 i;
-	unsigned int keymask;
-	unsigned int changed;
-	static unsigned int prev_keymask = 0;
-	struct keymap_entry *key;
-
-	outb(FJBTNS_DOCK_READ_STATE, FJBTNS_DOCK_WRITE);
-	fscbtns.docked = inb(FJBTNS_DOCK_READ);
-
-	IOWRITEB(0xdd, 0);
-	i = IOREADB(4) ^ 0xff;
-	if(i != fscbtns.orientation) {
-		pr_debug("orientation change (%d)", i);
-		fscbtns.orientation = i;
+	int r = fscbtns_read_register(0xdd) ^ 0xff;
+	if(r != fscbtns.orientation) {
+		pr_debug("orientation change (%d)\n", r);
 		input_report_switch(fscbtns.idev, SW_TABLET_MODE,
-				fscbtns.orientation);
+				fscbtns.orientation = r);
 	}
-
-	IOWRITEB(0xde, 0);
-	keymask = IOREADB(4) ^ 0xff;
-	IOWRITEB(0xdf, 0);
-	keymask |= (IOREADB(4) ^ 0xff) << 8;
-
-	changed = keymask ^ prev_keymask;
-	pr_debug("keymask: 0x%04x (0x%04x)\n", keymask, changed);
-
-	if(changed) {
-		for(key = fscbtns.keymap; key->mask; key++)
-			if(key->mask == changed) {
-				pr_debug("send %d %s", key->code, (keymask & changed ? "pressed" : "released\n"));
-				input_report_key(fscbtns.idev, key->code, !!(keymask & changed));
-				break;
-			}
-
-		prev_keymask = keymask;
-	}
-
-	input_sync(fscbtns.idev);
 }
 
+inline void fscbtns_handle_docking(void)
+{
+	outb(FJBTNS_DOCK_READ_STATE, FJBTNS_DOCK_WRITE);
+	fscbtns.docked = inb(FJBTNS_DOCK_READ);
+}
 
 /*** INTERRUPT ****************************************************************/
 
 static void fscbtns_isr_do(struct work_struct *work)
 {
-	fscbtns_event();
-	IOREADB(2);	
+	unsigned long keymask;
+	unsigned long changed;
+	static unsigned long prev_keymask = 0;
+
+	fscbtns_handle_docking();
+	fscbtns_handle_rotating();
+
+	keymask  = fscbtns_read_register(0xde);
+	keymask |= fscbtns_read_register(0xdf) << 8;
+	keymask ^= 0xffff;
+
+	changed = keymask ^ prev_keymask;
+	pr_debug("keymask: 0x%04lx (0x%04lx)\n", keymask, changed);
+
+	if(changed) {
+		int key = 0;
+		int pressed = !!(keymask & changed);
+
+		/* save current state and filter not changed bits */
+		prev_keymask = keymask;
+
+		/* get number of changed bit */
+		while(key < ARRAY_SIZE(default_keymap) && !test_bit(key, &changed))
+			key++;
+
+		if(key < ARRAY_SIZE(default_keymap)) {
+			/*XXX: useful? right code? both??? */
+			input_event(fscbtns.idev, EV_MSC, MSC_RAW, key);
+			input_event(fscbtns.idev, EV_MSC, MSC_SCAN, key);
+
+			pr_debug("send %d %s\n", fscbtns.keymap[key],
+					(pressed ? "pressed" : "released"));
+			input_report_key(fscbtns.idev, fscbtns.keymap[key],
+					pressed);
+		} else
+			pr_info("BUG? key > %d\n", ARRAY_SIZE(default_keymap));
+	}
+
+	input_sync(fscbtns.idev);
+
+	inb(FJBTNS_RESET_PORT);
 }
+
 static DECLARE_WORK(isr_wq, fscbtns_isr_do);
 
 static irqreturn_t fscbtns_isr(int irq, void *dev_id)
 {
-	int irq_me = IOREADB(6) & 0x01;
-	pr_debug("INTERRUPT (0:%d)\n", irq_me);
-
-	if(!irq_me)
-		return IRQ_NONE;
-
-	schedule_work(&isr_wq);
-	return IRQ_HANDLED;
+	if(IOREADB(FJBTNS_STATUS_PORT) & 0x01) {
+		schedule_work(&isr_wq);
+		return IRQ_HANDLED;
+	}
+	return IRQ_NONE;
 }
 
 
@@ -287,14 +339,34 @@ static irqreturn_t fscbtns_isr(int irq, void *dev_id)
 
 static int fscbtns_busywait(void)
 {
-	int timeout_counter = 100;
+	int timeout_counter = 255;
 
-	while(IOREADB(6) & 0x02 && --timeout_counter)
-		msleep(10);
+	while(IOREADB(FJBTNS_STATUS_PORT) & 0x02 && --timeout_counter)
+		udelay(100);
 
-	pr_debug("busywait done (rest: %d)\n", timeout_counter);
+	pr_debug("busywait rest: %d\n", timeout_counter);
 	return !timeout_counter;
 }
+
+static int fscbtns_reset(void)
+{
+	int timeout = 1024;
+
+	/* TODO: write machine specific reset seq. to regs 0xe8..0xea */
+	/* TODO: write 0 to status port */
+
+	do {
+		inb(FJBTNS_RESET_PORT);
+		if(!inb(FJBTNS_STATUS_PORT) & 1)
+			return 0;
+
+		udelay(100);
+	} while(--timeout);
+
+	printk(KERN_WARNING MODULENAME ": timeout during reset, this should not happen\n");
+	return -1;
+}
+
 
 static int __devinit fscbtns_probe(struct platform_device *dev)
 {
@@ -305,7 +377,6 @@ static int __devinit fscbtns_probe(struct platform_device *dev)
 	if(error)
 		return error;
 
-	/* TODO: mod parameters? */
 	fscbtns_set_repeat_rate(repeat_delay, 1000 / repeat_rate);
 
 	resource = request_region(FJBTNS_DOCK_BASE, 2, MODULENAME " (dock)");
@@ -326,19 +397,27 @@ static int __devinit fscbtns_probe(struct platform_device *dev)
 	dump_regs();
 #endif
 
+
+	inb(FJBTNS_RESET_PORT);
+	error = fscbtns_busywait();
+	if(error) {
+		pr_debug("reseting...\n");
+		error = fscbtns_reset();
+		if(error)
+			goto err_io2;
+	} else
+		pr_debug("device ready!\n");
+
+	fscbtns_handle_docking();
+	fscbtns_handle_rotating();
+	input_sync(fscbtns.idev);
+
 	error = request_irq(fscbtns.interrupt, fscbtns_isr,
 			IRQF_SHARED, MODULENAME, fscbtns_isr);
 	if(error) {
 		pr_err("unable to use irq %d\n", fscbtns.interrupt);
 		goto err_io2;
 	}
-
-	IOREADB(2);
-
-	if(!fscbtns_busywait())
-		pr_debug("device ready\n");
-	else
-		pr_debug("timeout, need a reset?\n");
 
 	return 0;
 
@@ -371,7 +450,7 @@ static int fscbtns_suspend(struct platform_device *dev, pm_message_t state)
 static int fscbtns_resume(struct platform_device *dev)
 {
 	pr_debug("resume:\n");
-	IOREADB(2);
+	IOREADB(FJBTNS_RESET_PORT);
 	return 0;
 }
 
@@ -464,28 +543,30 @@ static acpi_status fscbtns_walk_resources(struct acpi_resource *res, void *data)
 	}
 }
 
-static int acpi_fscbtns_add(struct acpi_device *device)
+static int acpi_fscbtns_add(struct acpi_device *dev)
 {
 	acpi_status status;
 
-	if(!device) {
-		pr_debug("acpi device not found");
+	if(!dev) {
+		pr_debug("acpi device not found\n");
 		return -EINVAL;
 	}
 
-	fscbtns.adev = device;
-
-	pr_debug("acpi: walking...\n");
-	status = acpi_walk_resources(device->handle, METHOD_NAME__CRS, fscbtns_walk_resources, NULL);
-	if(ACPI_FAILURE(status)) {
-		pr_debug("acpi walk failed");
-		return -ENODEV;
+	if(!fscbtns.keymap) {
+		if(!strcmp("FUJ02BD", acpi_device_hid(dev)))
+			fscbtns.keymap = keymap_Stylistic;
+		else if(!strcmp("FUJ02BF", acpi_device_hid(dev)))
+			fscbtns.keymap = keymap_Tseries;
 	}
+
+	status = acpi_walk_resources(dev->handle, METHOD_NAME__CRS, fscbtns_walk_resources, NULL);
+	if(ACPI_FAILURE(status))
+		return -ENODEV;
 
 	return 0;
 }
 
-static int acpi_fscbtns_remove(struct acpi_device *device, int type)
+static int acpi_fscbtns_remove(struct acpi_device *dev, int type)
 {
 	return 0;
 }
@@ -509,13 +590,15 @@ static int __init fscbtns_module_init(void)
 {
 	int error = -EINVAL;
 
-	// TODO: test
 	switch(user_keymap) {
 		case 1:
-			fscbtns.keymap = keymap_stylistic;
+			pr_info("force keymap for Stylistic Tablets");
+			fscbtns.keymap = keymap_Stylistic;
 			break;
-		default:
-			fscbtns.keymap = keymap_t4010;
+		case 2:
+			pr_info("force keymap for T-Series Tablet Lifebooks");
+			fscbtns.keymap = keymap_Tseries;
+			break;
 	}
 
 #ifdef CONFIG_ACPI
@@ -594,8 +677,9 @@ MODULE_PARM_DESC(rate, "repeat rate");
 module_param_named(delay, repeat_delay, uint, 0);
 MODULE_PARM_DESC(delay, "repeat delay");
 
+/*TODO: test to load a keymap by hal */
 module_param_named(keymap, user_keymap, uint, 0);
-MODULE_PARM_DESC(keymap, "keymap (0 = T4010, 1 = Stylistic)");
+MODULE_PARM_DESC(keymap, "keymap (1 = Stylistic, 2 = T- and P-Series)");
 
 
 static struct pnp_device_id pnp_ids[] = {
