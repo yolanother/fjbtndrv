59df2f235e151200d6836ee10568aa3e2e620915
khnz <khnz@4a459e26-1d7e-42ee-bc08-29a454924080>
2008-12-22 23:09:01 +0000

    fixed some licensing issues
     COPYING                      |  342 +++++++++++++++++++++++++++++++++++++++++++
     configure.in                 |    4 
     contrib/rotate-script.sample |    2 
     debian/copyright             |   30 +--
     po/de.po                     |    9 -
     src/fscd.c                   |   15 -
     src/fscd.h                   |   14 -
     src/fscrotd.c                |   15 -
     src/gui-nogui.c              |   15 -
     src/gui-xosd.c               |   23 +-
     src/gui.h                    |   14 -
     src/linux/fsc_btns.c         |    2 
     src/wacom.c                  |   13 -
     src/wacom.h                  |   14 -
     14 files changed, 413 insertions(+), 99 deletions(-)
    
    
    git-svn-id: https://fjbtndrv.svn.sourceforge.net/svnroot/fjbtndrv/trunk@353 4a459e26-1d7e-42ee-bc08-29a454924080

--- src/gui-xosd.c
+++ src/gui-xosd.c
@@ -1,10 +1,9 @@
-/* FSC Tablet Buttons Helper Daemon (simple OSD)
- * Copyright (C) 2007 Robert Gerlach
+/*
+ * Copyright (C) 2007-2008 Robert Gerlach
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * You can redistribute and/or modify this program under the terms of the
+ * GNU General Public License version 3 as published by the Free Software
+ * Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,15 +11,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-/******************************************************************************/
-
-#define XOSD_COLOR		"green"
-#define XOSD_OUTLINE_COLOR	"darkgreen"
-
-/******************************************************************************/
 
 #include "fscd.h"
 #include "gui.h"
@@ -34,6 +26,9 @@
 #else
 #  define _(x) (x)
 #endif
+
+#define XOSD_COLOR		"green"
+#define XOSD_OUTLINE_COLOR	"darkgreen"
 
 static xosd *osd = NULL;
 
