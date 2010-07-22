cc3c38b71c56588740362a7f9cffe579e0f7ff34
Robert Gerlach <khnz@users.sourceforge.net>
2010-07-22 04:46:38 +0200

    reworked for newer xorg/xinput releases

--- contrib/rotate-wacom.sh
+++ contrib/rotate-wacom.sh
@@ -1,36 +1,27 @@
 #!/bin/sh
 
-export LANG=C
-
-find_stylus () {
-	$xinput --list --short \
-	| cut -d\" -f2 \
-	| while read name; do
-		if $xinput --list "$name" | grep -q 'Type is Wacom Stylus'; then
-			echo "$name"
-			break
-		fi
-	done
-}
-
 test "$ACTION" = "rotated" || exit 0
-xsetwacom="`which xsetwacom`" || exit 0
-xinput="`which xinput`" || exit 0
 
 case "$ORIENTATION" in
-	n*) rotate=0 ;;
-	r*) rotate=1 ;;
-	l*) rotate=2 ;;
-	i*) rotate=3 ;;
+	n*) val=0 ;;
+	r*) val=1 ;;
+	l*) val=2 ;;
+	i*) val=3 ;;
 	*)  exit 1 ;;
 esac
 
-devname="`find_stylus`"
-if [ "$devname" ]; then
-	$xsetwacom set "$devname" rotate "$rotate"
-	$xsetwacom set "$devname" xyDefault
-else
-	echo "Wacom Stylus not found." >&2
-fi
+# xinput installed?
+xinput --version || exit 0
+
+export LC_ALL=C
+
+xinput list --short \
+| sed 's/^[^[:alnum:]]* \(.*\) *id=.*/\1/p; d' \
+| grep -vi 'virtual' \
+| while read dev; do
+	if xinput list-props "$dev" | grep -q 'Wacom Rotation'; then
+		xinput set-prop "$dev" 'Wacom Rotation' $val
+	fi
+done
 
 exit 0
